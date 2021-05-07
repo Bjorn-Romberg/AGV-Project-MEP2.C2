76881344bd8d0434a3f87c5d611d4bba6f6ec672
Björn Romberg <bjornrom99@gmail.com>
2021-05-07 08:50:33 +0200

    Verandering Test Code Björn Romberg

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
-/*
- */
-
 #include <avr/io.h>
+#define LED1EN PORTB |= (1<<PB6)
+
+// Testen GitHub
+int Test_Variabele_Bjorn_Romberg = 0;
+
+void init(void)
+{
+    DDRB |= (1<<PB6); //Output Led
+    PORTB |= (1<<PB6);
+}
+
 
 int main(void)
 {
+    init();
 
-    // Testen GitHub
+    Test_Variabele_Bjorn_Romberg ++;
 
-    while(1)
-    ;
- int github_werkt;
+    if(Test_Variabele_Bjorn_Romberg == 1)
+    {
+        LED1EN;
+        Test_Variabele_Bjorn_Romberg = 0;
+    }
     return 0;
 }
